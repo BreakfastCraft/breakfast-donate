e671c2ce8754dde9f6db5ab6d738ae596838c2a9
Bret Belgarde <bretbelgarde@gmail.com>
2014-06-28 18:46:18 -0400

    Adding files missed in last commit.

--- ipn-listener.php
+++ ipn-listener.php
@@ -2,50 +2,6 @@
 error_reporting(E_STRICT);
 require('ipn-message.php');
 
-header('HTTP/1.1 200 OK');
-
-$tester = array(
-    'mc_gross'                => '19.95',
-    'protection_eligibility'  => 'Eligible',
-    'address_status'          => 'confirmed',
-    'payer_id'                => 'LPLWNMTBWMFAY',
-    'tax'                     => '0.00',
-    'address_street'          => '1 Main St',
-    'payment_date'            => '20:12:59 Jan 13, 2009 PST',
-    'payment_status'          => 'Completed',
-    'charset'                 => 'windows-1252',
-    'address_zip'             => '95131',
-    'first_name'              => 'Test',
-    'mc_fee'                  => '0.88',
-    'address_country_code'    => 'US',
-    'address_name'            => 'Test User',
-    'notify_version'          => '2.6',
-    'custom'                  => '',
-    'payer_status'            => '',
-    'verifiedaddress_country' => 'United States',
-    'address_city'            => 'San Jose',
-    'quantity'                => '1',
-    'verify_sign'             => 'AtkOfCXbDm2hu0ZELryHFjY-Vb7PAUvS6nMXgysbElEn9v-1XcmSoGtf',
-    'payer_email'             => 'gpmac_1231902590_per@paypal.com',
-    'txn_id'                  => '61E67681CH3238416',
-    'payment_type'            => 'instant',
-    'last_name'               => 'User',
-    'address_state'           => 'CA',
-    'receiver_email'          => 'gpmac_1231902686_biz@paypal.com',
-    'payment_fee'             => '0.88',
-    'receiver_id'             => 'S8XGHLYDW9T3S',
-    'txn_type'                => 'express_checkout',
-    'item_name'               => '',
-    'mc_currency'             => 'USD',
-    'item_number'             => '',
-    'residence_country'       => 'US',
-    'test_ipn'                => '1',
-    'handling_amount'         => '0.00',
-    'transaction_subject'     => '',
-    'payment_gross'           => '19.95',
-    'shipping'                => '0.00'
-);
-
 //Process IPN
 $message = new BreakfastCraft\IPNMessage($_POST);
 
@@ -54,9 +10,6 @@ if ($message->isIPNValid()) {
 } else {
     $error = array(
         'error'      => 'Bad stuff has happend!',
-        'ipn'        => $message->ipn,
-        'sketchyIPN' => $message->sketchyIPN,
-        'response'   => $message->response
     );
     file_put_contents('messages.json', json_encode($error));
 }
--- ipn-message.php
+++ ipn-message.php
@@ -4,26 +4,27 @@ namespace BreakfastCraft;
 class IPNMessage
 {
 
-    public $ipn;
-    public $sketchyIPN;
-    public $message_status;
-    public $fsockerr = array(
-        'errno'  => '',
-        'errstr' => ''
-    );
+    //TODO: Switch these back to private after everything works
 
-    private $paypalURL = 'www.sandbox.paypal.com';
+    private $ipn;
+    private $sketchyIPN;
+    private $message_status;
+    private $paypalURL;
     private $mode;
     private $filename = 'messages.json';
+    private $logfile = 'ipn.log';
 
     public function __construct($ipn, $mode = 'sandbox')
     {
         $this->ipn = $ipn;
         
         if ($mode != 'sandbox') {
-            $this->paypalURL = 'www.paypal.com';
+            $this->paypalURL = "https://www.paypal.com/cgi-bin/webscr";
+        } else {
+            $this->paypalURL = "https://www.sandbox.paypal.com/cgi-bin/webscr";
         }
 
+        $this->mode = $mode;
     }
 
     public function isIPNValid()
@@ -47,20 +48,38 @@ class IPNMessage
 
     private function postIPN()
     {
-        // POST validation request to PayPal
-        $header = "POST /cgi-bin/webscr HTTP/1.1\r\n";
-        $header .= "Host: " . $this->paypalURL ."\r\n";
-        $header .= "Content-Type: application/x-www-form-urlencoded\r\n";
-        $header .= "Content-Length: " . strlen($this->sketchyIPN) . "\r\n";
-        $header .= "Connection: Close\r\n\r\n";
-
-        $post = fsockopen("ssl://" . $this->paypalURL . "/", 443, $errno, $errstr, 30);
-        fputs($post, $header . $this->sketchyIPN);
-
+        $ch = curl_init($this->paypalURL);
         
-        $response = stream_get_contents($post, 1024);
-        return $response;
-        
+        if ($ch == false) {
+            return false;
+        }
+
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->sketchyIPN);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+
+        if ($this->mode == 'sandbox') {
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+            curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+        }
+
+        curl_setopt($ch, CURLOPT_CONNNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, arra('Connection: Close'));
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        if (strcmp($response, "VERIFIED") === 0) {
+            return true;
+        }
+
+        return false;
+
     }
 
     public function writeMessage()
